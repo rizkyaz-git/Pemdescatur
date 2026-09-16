57b38d66c33542096fcee99cf1b20d772981a554
rizkyaz-git <a710239140@student.ums.ac.id>
2026-09-16 11:08:03 +0700

    Add graceful fallback to HtmlPurifierHelper

--- app/Helpers/HtmlPurifierHelper.php
+++ app/Helpers/HtmlPurifierHelper.php
@@ -21,37 +21,73 @@ class HtmlPurifierHelper
             return '';
         }
 
-        $config = HTMLPurifier_Config::createDefault();
+        // Graceful fallback jika package ezyang/htmlpurifier belum terpasang di vendor (misal di server hosting belum composer install)
+        if (!class_exists(\HTMLPurifier_Config::class) || !class_exists(\HTMLPurifier::class)) {
+            return self::fallbackClean($html);
+        }
 
-        // Izinkan elemen HTML yang umum dipakai oleh Quill editor
-        $config->set('HTML.Allowed',
-            'h1,h2,h3,h4,h5,h6,p[style],ul,ol,li,blockquote,'
-            . 'a[href|target|rel],strong,b,em,i,u,s,del,br,'
-            . 'img[src|alt|width|height|class|style],'
-            . 'table,thead,tbody,tr,td[colspan|rowspan|style],th[colspan|rowspan|style],'
-            . 'hr,code,pre,span[class|style],div[class|style]'
-        );
+        try {
+            $config = HTMLPurifier_Config::createDefault();
 
-        // Izinkan style terbatas (Quill sering memakai inline style)
-        $config->set('CSS.AllowedProperties', [
-            'text-align', 'color', 'background-color', 'font-size',
-            'font-weight', 'font-style', 'text-decoration',
-            'margin', 'margin-top', 'margin-bottom', 'margin-left', 'margin-right',
-            'padding', 'padding-top', 'padding-bottom', 'padding-left', 'padding-right',
-            'border', 'border-radius', 'width', 'max-width', 'height',
-            'float', 'display', 'list-style-type', 'line-height',
-        ]);
+            // Izinkan elemen HTML yang umum dipakai oleh Quill editor
+            $config->set('HTML.Allowed',
+                'h1,h2,h3,h4,h5,h6,p[style],ul,ol,li,blockquote,'
+                . 'a[href|target|rel],strong,b,em,i,u,s,del,br,'
+                . 'img[src|alt|width|height|class|style],'
+                . 'table,thead,tbody,tr,td[colspan|rowspan|style],th[colspan|rowspan|style],'
+                . 'hr,code,pre,span[class|style],div[class|style]'
+            );
 
-        // Paksa rel="noopener noreferrer" pada link agar aman
-        $config->set('HTML.TargetBlank', true);
-        $config->set('HTML.TargetNoreferrer', true);
+            // Izinkan style terbatas (Quill sering memakai inline style)
+            $config->set('CSS.AllowedProperties', [
+                'text-align', 'color', 'background-color', 'font-size',
+                'font-weight', 'font-style', 'text-decoration',
+                'margin', 'margin-top', 'margin-bottom', 'margin-left', 'margin-right',
+                'padding', 'padding-top', 'padding-bottom', 'padding-left', 'padding-right',
+                'border', 'border-radius', 'width', 'max-width', 'height',
+                'float', 'display', 'list-style-type', 'line-height',
+            ]);
 
-        // Simpan cache purifier di storage
-        $config->set('Cache.SerializerPath', storage_path('app/htmlpurifier'));
+            // Paksa rel="noopener noreferrer" pada link agar aman
+            $config->set('HTML.TargetBlank', true);
+            $config->set('HTML.TargetNoreferrer', true);
 
-        $purifier = new HTMLPurifier($config);
+            // Pastikan direktori cache serializer ada dan bisa ditulisi
+            $cachePath = storage_path('app/htmlpurifier');
+            if (!is_dir($cachePath)) {
+                @mkdir($cachePath, 0775, true);
+            }
+            if (is_dir($cachePath) && is_writable($cachePath)) {
+                $config->set('Cache.SerializerPath', $cachePath);
+            } else {
+                $config->set('Cache.DefinitionImpl', null);
+            }
 
-        return $purifier->purify($html);
+            $purifier = new HTMLPurifier($config);
+
+            return $purifier->purify($html);
+        } catch (\Throwable $e) {
+            report($e);
+            return self::fallbackClean($html);
+        }
+    }
+
+    /**
+     * Pembersihan darurat (fallback) jika HTMLPurifier belum diinstal via composer pada hosting/server.
+     */
+    private static function fallbackClean(string $html): string
+    {
+        $allowedTags = '<p><h1><h2><h3><h4><h5><h6><ul><ol><li><a><strong><b><em><i><u><s><del><br><img><table><thead><tbody><tr><td><th><hr><code><pre><span><div>';
+        $cleaned = strip_tags($html, $allowedTags);
+
+        // Hapus event handler JavaScript (onclick, onerror, onload, onmouseover, dll)
+        $cleaned = preg_replace('/\s+on[a-zA-Z]+\s*=\s*(["\'][^"\']*["\']|[^\s>]+)/i', '', $cleaned);
+
+        // Netralkan link javascript:
+        $cleaned = preg_replace('/href\s*=\s*["\']\s*javascript:[^"\']*["\']/i', 'href="#"', $cleaned);
+        $cleaned = preg_replace('/src\s*=\s*["\']\s*javascript:[^"\']*["\']/i', '', $cleaned);
+
+        return $cleaned;
     }
 
     /**
